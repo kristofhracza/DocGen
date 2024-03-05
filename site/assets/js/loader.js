73238d92c18711237c49a08437b09c051eb06cf8
IVBecy <kristofhracza@gmail.com>
2024-03-05 00:52:21 +0000

    Swicthed vanilla to REACT for better and easier rendering

--- site/assets/js/loader.js
+++ site/assets/js/loader.js
@@ -1,67 +1,6 @@
 "use-strict"
 
-// Function to make list entries
-const makeEntry = (url, text, isMain) => {
-    const newText = document.createElement("li");
-    newText.textContent = text;
-    newText.dataset.ref = url;
-    if (isMain === false) newText.onclick = () => {
-        document.querySelector("#page-render").src = url
-        document.title = "DocGen " + text;
-        localStorage.setItem("lastPage",url)
-    }
-    if (isMain) newText.className = "main-header";
-    else newText.className = "header";
-    return newText;
-}
-
-// Duties on page load
-window.onload = () => {
-    let sideBar = document.querySelector(".sidebar");
-
-    // Handle JSON data of existing pages
-    const parseJsonLinks = () => {
-        fetch("../LINKS.json")
-        .then(response => response.json())
-        .then(json => {
-            // Loop raw JSON data
-            for (const tree in json) {
-                let link = json[tree];
-                let structure = tree.split("/");
-                let list = sideBar;
-                let subList;
-
-                // Loop current structure
-                for (let i = 0; i < structure.length; i++) {
-                    let currentText = structure[i];
-                    let currentID = structure.slice(0, i + 1).join("-");
-                    subList = Array.from(list.querySelectorAll("ul")).find(ul => ul.id === currentID);
-
-                    // Checks and list creation
-                    if (currentText === "MAIN") continue;
-                    if (!subList) {
-                        let isMain = (currentText === currentID && currentText !== "README");
-                        subList = document.createElement("ul");
-                        subList.id = currentID;
-                        subList.appendChild(makeEntry(link, currentText,isMain));
-                        list.appendChild(subList);
-                    }
-
-                    // Update list
-                    list = subList;
-                }
-            }
-        });
-    }
-    parseJsonLinks();
-
-    // Set page to last visited url
-    let lastPage = localStorage.getItem("lastPage");
-    if (lastPage){
-        document.querySelector("#page-render").src = lastPage;
-    }
-
-    // Trigger menu
+const hamburger = () => {
     const hamburger = document.querySelector(".hamburger");
     const sidebar = document.querySelector(".sidebar")
     const icon = document.querySelector("i")
@@ -70,7 +9,6 @@ window.onload = () => {
         sidebar.style.width = sidebar.style.width == "75%" ? "0" : "75%"
         icon.className = icon.className == "fa fa-bars" ? "fa fa-times" : "fa fa-bars"
 
-        // Fetch chosen page, then close menu
         if (icon.className == "fa fa-times") {
             document.querySelectorAll(".header").forEach(header => {
                 header.onclick = () => {
@@ -81,4 +19,23 @@ window.onload = () => {
             });
         }
     }
+}
+
+window.onload = () => {
+    let sideBar = document.querySelector(".sidebar");
+    let rootSide = ReactDOM.createRoot(sideBar);
+
+    const parseJsonLinks = () => {
+        fetch("../LINKS.json")
+        .then(response => response.json())
+        .then(json => {
+            rootSide.render(renderSummary(json))
+        });
+    }
+
+    parseJsonLinks();
+    hamburger();
+
+    let lastPage = localStorage.getItem("lastPage");
+    if (lastPage) document.querySelector("#page-render").src = lastPage;
 }
---
+++ site/assets/js/reactHooks.js
@@ -0,0 +1,36 @@
+const listClick = (url,text) => (event) => {
+    document.title = "DocGen - " + text;
+    document.querySelector("#page-render").src = url
+    localStorage.setItem("lastPage",url)
+}
+
+// It's an understatement to say that this is a clusterfuck (but it works)
+// Here we loop the whole LINKS file and produce a lovely output on the sidebar
+const renderSummary = (summary) => {
+    return (
+        Object.entries(summary).map(([head, link]) => (
+            <ul head={head}>
+                {typeof link === "object" ? (
+                <>
+                    {(Object.keys(link)[0] === "MAIN") ? (
+                        <>
+                            <h6 className="main-list" id={head} onClick={listClick(Object.values(link)[0],Object.keys(link)[0])}>{head}</h6>
+                            {Object.keys(link).length > 1 ? delete link["MAIN"] && renderSummary(link) : null}
+                        </>
+                        ) :
+                        (
+                        <>
+                            <h6 id={head}>{head}</h6>
+                            {renderSummary(link)}
+                        </>
+                        )}
+                    </>
+                ) : (
+                    <>
+                        <li data-ref={link} onClick={listClick(link,head)}>{head}</li>
+                    </>
+                )}
+            </ul>
+        ))
+    );
+  };
